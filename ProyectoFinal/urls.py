eb522ee8edb29ef6475da2d27792c45684cc5a10
Dubraska Salcedo <dubraskasalcedo@gmail.com>
2023-04-09 23:43:54 -0400

    Modificación de la base, esencialmente limpiando la plantilla; y agregando Avatar, cambios en formulario de registro y edición

--- ProyectoFinal/urls.py
+++ ProyectoFinal/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from AppProyectoFinal.views import mostrar_principal
 from django.conf.urls.static import static
-from ProyectoFinal import settings
+from django.conf import settings
+
 
 
 urlpatterns = [
